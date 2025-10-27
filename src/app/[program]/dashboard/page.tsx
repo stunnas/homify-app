519e2944c4113b35cac4261c75abe3c78c9de3e1
Chase Albritton <caa.develops@gmail.com>
2025-10-26 22:55:14 -0400

    remove type for props and define within function

--- src/app/[program]/dashboard/page.tsx
+++ src/app/[program]/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { redirect } from 'next/navigation';
 import { getDefaultRouteForProgram } from '@/lib/data/nav-data';
 
-type DashboardPageProps = {
-	params: { program: string };
-};
 
-export default function DashboardRootPage({ params }: DashboardPageProps) {
+export default function DashboardRootPage({
+	params,
+}: {
+	params: { program: string };
+}) {
 	const defaultRoute = getDefaultRouteForProgram(params.program);
 	redirect(defaultRoute);
 
